549c1004644bf59a74a22c0efab609f13c535947
Davit Tabidze <davidtabidzef1@gmail.com>
2023-08-06 16:09:52 -0400

    carid

--- server/app.py
+++ server/app.py
@@ -29,3 +29,28 @@ def cars():
             return car.to_dict(), 201
         except (IntegrityError, ValueError) as ie:
             return {"error": ie.args}, 422
+
+
+@app.route("/cars/<int:id>", methods=["GET", "PATCH", "DELETE"])
+def get_car_by_id(id):
+    car = Car.query.filter(Car.id == id).first()
+    if not car:
+        return {}, 404
+    elif request.method == "GET":
+        return car.to_dict(), 200
+    elif request.method == "PATCH":
+        data = request.json
+        try:
+            for key in data:
+                setattr(car, key, data[key])
+            db.session.commit()
+            return car.to_dict(), 200
+        except (IntegrityError, ValueError) as ie:
+            return {"error": ie.args}, 422
+    elif request.method == "DELETE":
+        if car.status == "sold":
+            return {"error": "can't delete sold car"}, 403
+        else:
+            db.session.delete(car)
+            db.session.commit()
+            return {"message": "Car deleted successfully"}, 200
