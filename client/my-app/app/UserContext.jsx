6af83f76f8312db2641fa7953a57f238a4910269
Avi Kaul <avikarkaul@gmail.com>
2023-08-10 23:06:13 -0400

    merged

--- client/my-app/app/UserContext.jsx
+++ client/my-app/app/UserContext.jsx
@@ -1,15 +1,3 @@
-import { createContext, useContext, useState } from "react";
-
-export const UserContext = createContext();
-
-export function UserProvider({ children }) {
-  const [loggedInUser, setLoggedInUser] = useState({});
-  return (
-    <UserContext.Provider value={{ loggedInUser, setLoggedInUser }}>
-      {children}
-    </UserContext.Provider>
-  );
-}
 import { createContext, useEffect, useState } from "react";
 
 export const UserContext = createContext();
