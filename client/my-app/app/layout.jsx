d7809635d3270ee562418fc7684356f9dbed9ae0
Avi Kaul <avikarkaul@gmail.com>
2023-08-10 23:26:32 -0400

    working merge

--- client/my-app/app/layout.jsx
+++ client/my-app/app/layout.jsx
@@ -1,18 +1,42 @@
+"use client"
 import "@styles/globals.css";
-import Nav from "./Nav/Nav";
+// import Nav from "./Nav/Nav";
 
-export const metadata = {
-  title: "Cars",
-  description: "Trade or Buy Cars",
-};
+// export const metadata = {
+//   title: "Cars",
+//   description: "Trade or Buy Cars",
+// };
+// const RootLayout = ({ children }) => {
+//   return (
+//     <html lang="en">
+//       <body>
+//         <Nav />
+//         <main>{children}</main>
+//       </body>
+//     </html>
+//   );
+// };
+
+// export default RootLayout;
+import React from 'react';
+import Nav from './Nav/Nav';
+import { UserProvider } from './UserContext'; // Import the UserProvider
+
+// export const metadata = {
+//   title: "Cars",
+//   description: "Trade or Buy Cars",
+// };
+
 const RootLayout = ({ children }) => {
   return (
-    <html lang="en">
-      <body>
-        <Nav />
-        <main>{children}</main>
-      </body>
-    </html>
+    <UserProvider>
+      <html lang="en">
+        <body>
+          <Nav />
+          <main>{children}</main>
+        </body>
+      </html>
+    </UserProvider>
   );
 };
 
